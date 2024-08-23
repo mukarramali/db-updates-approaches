bf946b32aafa3661074726d0f31e4363be680cf3
Mukarram Ali <mukarram.ali89@gmail.com>
2024-08-23 08:15:09 +0200

    wip

--- test/load.go
+++ test/load.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	CONCURRENT_USERS        = 50
+	CONCURRENT_USERS        = 400
 	CONCURRENCY_MAX_LATENCY = 10 * time.Millisecond
 	strategy                = "SelectAndUpdate"
 	apiURL                  = "http://localhost:8080?type=%s"
@@ -19,12 +19,15 @@ func sleepRandomLatency() {
 	time.Sleep(time.Duration(rand.Intn(int(CONCURRENCY_MAX_LATENCY))))
 }
 
-func makeRequest(wg *sync.WaitGroup, successCount, rejectCount *int) {
+func makeRequest(wg *sync.WaitGroup, successCount *int, failedRequests *map[int64]int64) {
 	defer wg.Done()
 	sleepRandomLatency()
 
 	resp, err := http.Post(fmt.Sprintf(apiURL, strategy), "application/json", nil)
 	if err != nil || resp.StatusCode >= 400 {
+		if failedRequests[resp.StatusCode] == nil {
+			failedRequests[resp.StatusCode]
+		}
 		*rejectCount++
 		return
 	}
@@ -35,10 +38,11 @@ func main() {
 	var wg sync.WaitGroup
 	successCount := 0
 	rejectCount := 0
+	failedRequests := map[int]int{}
 	wg.Add(CONCURRENT_USERS)
 
 	for i := 0; i < CONCURRENT_USERS; i++ {
-		go makeRequest(&wg, &successCount, &rejectCount)
+		go makeRequest(&wg, &successCount, &failedRequests)
 	}
 
 	wg.Wait()
